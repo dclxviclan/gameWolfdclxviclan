2cfbc0a182e0261d087c218856dcc0a99c5bc5f4
dclxviclan <109929818+dclxviclan@users.noreply.github.com>
2024-10-20 10:09:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -111,8 +111,13 @@ window.addEventListener('load', function(){
 			}  else if (input.keys.indexOf('ArrowUp') > -1 || input.keys.indexOf('swipe up') > -1 && this.onGround()){
 				this.vy -= 28;
 			} 
+				
+			else if (input.keys.indexOf('ArrowDown') > -1 || input.keys.indexOf('swipe down') > -1 && this.onGround()){
+				this.vy += 28;
+			} 	
 			else {
 				this.speed = 0;
+				this.vy = 0;
 			}
 			//horizontal movement 
 			this.x += this.speed;
